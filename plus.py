0499897d5d2e2cbf6a0f2e1579ff0721b2df77d1
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-07-16 09:53:05 +0530

    Create plus.py

---
+++ plus.py
@@ -0,0 +1,7 @@
+n=int(input())
+a=list(map(int,input().split()[:a1]))
+for i in range(len(a)):
+    for j in range(len(a)):
+        for k in range(len(a)):
+            if((a[i]+a[j]==a[k])and(i<j<k)):
+                print(a[i],a[j],a[k])
