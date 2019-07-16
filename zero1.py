86c2ff254db4608cca404c99a777043360bc9858
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-07-16 10:31:51 +0530

    Create zero1.py

---
+++ zero1.py
@@ -0,0 +1,10 @@
+n=int(input())
+a=list(map(int,input().split()))
+x=max(a)
+for i in range(len(a)):
+    for j in range(len(a)):
+        if((a[i]+a[j]<x)):
+            x1=a[i]
+            x2=a[j]
+            x=abs(x1+x2)
+print(x1,x2)  
