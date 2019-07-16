7da44759984dd4a16ecebd0816f18d05c0d7ff09
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-07-16 09:55:31 +0530

    Update plus.py

--- plus.py
+++ plus.py
@@ -1,6 +1,6 @@
 #sanjay
 n=int(input())
-a=list(map(int,input().split()[:a1]))
+a=list(map(int,input().split()))
 for i in range(len(a)):
     for j in range(len(a)):
         for k in range(len(a)):
